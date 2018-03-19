aaf17e817cba8eb1eb7dfa21463013a6790eb7e4
progettoingesoftware <34620499+progettoingesoftware@users.noreply.github.com>
2018-03-19 19:19:06 +0100

    Add files via upload

--- Costanti.java
+++ Costanti.java
@@ -1,4 +1,4 @@
-package it.ing.sw;
+package utility;
 
 import java.io.Serializable;
 
@@ -117,6 +117,7 @@ public class Costanti implements Serializable
 	public static final String SCELTA_INTERROGAZIONE = "Quale delle seguenti interrogazioni desideri fare all'archivio?\n1-Numero di prestiti per anno solare\n2-Numero di proroghe per anno solare\n3-Risorsa piu' richiesta per anno solare\n4-Numero di prestiti per fruitore per anno solare\nDigitare un numero:\n";
 	public static final int NUM_MASSIMO_SCELTA = 4;
 	public static final String INS_ANNO_RICHIESTO = "Inserisci l'anno:";
+	public static final int ANNO_MINIMO_INTERROGAZIONE = 1990;
 	public static final String INS_FRUITORE_RICHIESTO = "Inserisci lo username del fruitore: ";
     public static final String FRUITORE_NON_TROVATO = "Il fruitore selezionato non è presente nell'anagrafica\n";   
     public static final String NUM_PRESTITI_PER_ANNO = "Il numero di prestiti per l'anno selezionato è: ";
